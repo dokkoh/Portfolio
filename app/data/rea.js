4162cd6875cd2c1c0cbf45b1de3fd1711b386c1c
dokkoh <ceedje74@gmail.com>
2023-04-11 12:04:52 +0200

    création model

---
+++ app/data/rea.js
@@ -0,0 +1,12 @@
+import Rea from "../models/Rea.js";
+
+const reas = [
+  new Rea({
+    img: "test",
+    nom: "nom du site",
+    description: "description du site",
+    adrsse: "https://oclock.io/",
+  }),
+];
+
+export default reas;
---
+++ app/models/Rea.js
@@ -0,0 +1,17 @@
+class Rea {
+  img;
+  nom;
+  description;
+  adresse;
+
+  constructor(img, nom, description, adresse) {
+
+    this.img = img;
+    this.nom = nom;
+    this.description = description;
+    this.adresse = adresse;
+
+  }
+}
+
+export default Rea;
